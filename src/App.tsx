8cd9543047142b5ab923dc4423b453bfe20e5e6f
Alexeu Yakovlev <alexeu.23@mail.ru>
2024-07-28 21:42:34 +0300

    CardBlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardBlock from './components/card_component/CardBlock';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import MainPage from './Page/MainPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit src/App.tsx and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Page/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardBlock from "../components/card_component/CardBlock";
+import './MainPage.css'
+
+const MainPage: React.FC = () => {
+    return (
+        <div className="MainBlock">
+            <CardBlock />
+        </div>
+    );
+};
+export default MainPage;
